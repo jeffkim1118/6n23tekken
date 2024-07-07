b0b745be4a3d415bae698809ca90fb71e7c3e0ed
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2024-07-06 22:41:50 -0400

    Initial commit
    Initial commit with a working ps controller button detection feature.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PsPad from "./components/imgs/psPad";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [controllerIndex, setControllerIndex] = useState(null);
+
+  const updateControllerIndex = (index) => {
+    setControllerIndex(index);
+  };
+
+  useEffect(() => {
+    window.addEventListener("gamepadconnected", (event) => {
+      const gamepad = event.gamepad;
+      updateControllerIndex(gamepad.index);
+      console.log("connected");
+    });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("gamepaddisconnected", (event) => {
+      updateControllerIndex(null);
+      console.log("disconnected");
+    });
+  }, []);
+
+  const handleButtons = (buttons) => {
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons[i];
+      const buttonElement = document.getElementById(`controller-b${i}`);
+
+      const selectedButtonClass = "selected-button";
+      if (buttonElement) {
+        if (button.value > 0) {
+          buttonElement.classList.add(selectedButtonClass);
+        } else {
+          buttonElement.classList.remove(selectedButtonClass);
+        }
+      }
+    }
+  };
+
+  const gameLoop = () => {
+    if (controllerIndex !== null) {
+      const gamepad = navigator.getGamepads()[controllerIndex];
+      handleButtons(gamepad.buttons);
+    }
+    requestAnimationFrame(gameLoop);
+  }
+
+  useEffect(() => {
+    gameLoop();
+  },[controllerIndex]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="controller-container">
+          <PsPad />
+        </div>
       </header>
     </div>
   );
